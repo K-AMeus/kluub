c5d5f8cc50f967dc7e7f84e138b9072421d118d5
Karl-Andreas Meus <karl.andreas@meus.ee>
2025-12-14 18:25:04 +0200

    Fix favicon not showing up on Google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Kluub - Discover Events',
   description: 'Discover the perfect event for every occasion',
+  appleWebApp: {
+    title: 'Kluub',
+  },
 };
 
 export default function RootLayout({
